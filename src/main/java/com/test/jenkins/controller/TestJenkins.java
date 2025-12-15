c13b69d8b731f6c3bdd98cbe4a347a0b332535de
srini2107 <srinivasalu2107@gmail.com>
2025-12-15 20:12:17 +0530

    first commit

---
+++ src/main/java/com/test/jenkins/controller/TestJenkins.java
@@ -0,0 +1,14 @@
+package com.test.jenkins.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestJenkins {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Jenkins test successful!";
+    }
+}
